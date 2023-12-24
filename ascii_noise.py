35fcc105a698e704ed640fef6927cdc7b6d3c160
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-24 19:13:04 +0100

    cycling noise

--- ascii_noise.py
+++ ascii_noise.py
@@ -26,7 +26,8 @@ MAGENTA = display.create_pen(255, 33, 140)
 CYAN = display.create_pen(33, 177, 255)
 
 FONTS = ["bitmap6", "bitmap8", "bitmap14_outline", "sans", "gothic", "cursive", "serif", "serif_italic"]
-ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+# ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ascii_chars = "$@B%8&MW#*haokbdpqwmZO0QLJCJYXzcvunxrjft/\\|)(1}{][?-_+~i!lI;:,\"^`"
 
 # generate a list of pens with varying brightness values
 magenta = colours.Colour(255, 33, 140)
@@ -40,38 +41,23 @@ def get_char_at_index(string, float_index):
         index = len(string) - 1
     return string[index]
 
-# fill the screen with ascii characters
-def ascii_noise(scale=1):
-    x = 0
-    y = 0
-    columns = (WIDTH // (9 * scale)) + 1
-    rows = HEIGHT // (9 * scale)
-    for i in range(columns * rows):
-        # ic = random.randint(0, 128) / 127.0
-        ic = (noise(x/1.0, y/1.0) + 1.0) / 2.0
-        char = get_char_at_index(ascii_chars, ic)
-        # display.set_pen(VIOLET)
-        # display.rectangle(x, y, 8*scale, 8*scale)
-        # display.set_pen(MAGENTA)
-        display.text(char, x, y, scale=scale)
-        x += 9 * scale
-        if x > WIDTH:
-            x = 0
-            y += 9 * scale
-
 # fill the screen with pixels
-def pixel_noise(scale=1, x0=0, y0=0):
+def pixel_noise(scale=1, zoom=4, x0=0, y0=0, ascii=True, pixels=False):
     x = 0
     y = 0
     columns = (WIDTH // (8 * scale)) #+ 1
     rows = HEIGHT // (8 * scale) + 1
     for i in range(columns * rows):
-        ic = (noise(x/500.0 + x0, y/500.0 + y0) + 1.0) / 2.0
+        ic = (noise(x/(10.0 * zoom) + x0, y/(10.0 * zoom )+ y0) + 1.0) / 2.0
         # ic = random.randint(0, 127) / 127.0
         ix = int(ic * (len(PENS)-1))
         # print(ix)
         display.set_pen(PENS[ix])
-        display.rectangle(x, y, 8*scale, 8*scale)
+        if pixels:
+            display.rectangle(x, y, 8*scale, 8*scale)
+        if ascii:
+            char = get_char_at_index(ascii_chars, ic)
+            display.text(char, x, y, scale=scale)
         x += 8 * scale
         if x > WIDTH:
             x = 0
@@ -79,16 +65,22 @@ def pixel_noise(scale=1, x0=0, y0=0):
 
 display.set_font(FONTS[1])
 
-t = 0
+r = 1
+theta = 0
+theta_inc = 0.025
+
 while True:
-# draw background
-  display.set_pen(BLACK)
-  display.clear()
+    # draw background
+    display.set_pen(BLACK)
+    display.clear()
 
-  pixel_noise(3, t)
-  t += 0.025
+    x = r * sin(theta)
+    y = r * cos(theta)
+    theta = (theta + theta_inc) % (pi * 2)
 
-  display.update()
-  time.sleep(0.025)  # this number is how frequently Tufty checks for button presses
+    pixel_noise(3, 100, x, y)
+
+    display.update()
+    time.sleep(0.025)  # this number is how frequently Tufty checks for button presses
 
 
