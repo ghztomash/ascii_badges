52ae1e5bc5fd3080a4618e5cc46ba6e3d72f302b
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-24 04:35:03 +0100

    Modularize

--- ascii_matrix.py
+++ ascii_matrix.py
@@ -2,6 +2,7 @@
 import random
 import time
 import colours
+from matrix import Matrix
 
 from picographics import PicoGraphics, DISPLAY_TUFTY_2040
 display = PicoGraphics(display=DISPLAY_TUFTY_2040)
@@ -27,6 +28,7 @@ CYAN = display.create_pen(33, 177, 255)
 # generate a list of pens with varying brightness values
 magenta = colours.Colour(255, 33, 140)
 PENS = magenta.create_fade(display, 8)
+PENS.insert(0, WHITE)
 
 FONTS = ["bitmap6", "bitmap8", "bitmap14_outline", "sans", "gothic", "cursive", "serif", "serif_italic"]
 CHARACTER_HEIGHTS = [6, 8, 14, 8, 8, 8, 8, 8]
@@ -37,78 +39,8 @@ FALLING_SPEED = 20
 # The max number of falling rains. In config, we change it according to screen.
 MAX_RAIN_COUNT = 15
 
-def get_chars():
-    l = [i for i in range(33, 127)]
-    # half-width katakana. See https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms
-    # l.extend([chr(i) for i in range(0xFF66, 0xFF9D)])
-    return l
-
-CODE_CHARS = get_chars()
-
-def random_char():
-    return random.choice(CODE_CHARS)
-
-def random_rain_length():
-    return random.randint(5, 30)
-
-def random_rain_x():
-    return random.randint(0, WIDTH)
-
-def random_rain_speed():
-    return random.randint(5, FALLING_SPEED)
-
-class rain_drop:
-    def __init__(self, colour):
-        self.reset()
-        self.colour = colour
-
-    def reset(self):
-        self.x = random_rain_x()
-        self.y = 0
-        self.length = random_rain_length()
-        self.speed = random_rain_speed()
-        self.chars = [random_char()]
-        self.scale = random.randint(1, 2)
-        self.char_height = self.scale * 8
-
-    def move(self):
-        self.y += self.speed
-
-    def draw(self):
-        # draw head
-        display.set_pen(WHITE)
-        self.chars.insert(0, random_char())
-        if len(self.chars) > self.length:
-            self.chars.pop()
-        display.character(self.chars[0], self.x, self.y, scale=self.scale)
-        # draw tail
-        length = len(self.chars)
-        for i in range(1, length):
-            ci = int(i / length * (len(self.colour) - 1)) 
-            display.set_pen(self.colour[ci])
-            display.character(self.chars[i], self.x, self.y - (i * self.char_height), scale=self.scale)
-
-    def is_offscreen(self):
-        return self.y > HEIGHT + self.length * self.char_height
-    
-    def tick(self):
-        self.move()
-        self.draw()
-        if self.is_offscreen():
-            self.reset()
-
-class Matrix:
-    def __init__(self, max, pens):# array of rain drops
-        self.drops = []
-        for _ in range(max):
-            self.drops.append(rain_drop(pens))
-
-    def draw(self):
-        for drop in self.drops:
-            drop.tick()
-
 display.set_font(FONTS[1])
-matrix = Matrix(MAX_RAIN_COUNT, PENS)
+matrix = Matrix(display, MAX_RAIN_COUNT, PENS, FALLING_SPEED)
 
 while True:
 # draw background
--- lib/matrix/__init__.py
+++ lib/matrix/__init__.py
@@ -1,41 +1,34 @@
 import random
-
-def random_char():
-    return random.randint(33, 127)
-
-def random_rain_length():
-    return random.randint(5, 30)
-
-def random_rain_x():
-    return random.randint(0, WIDTH)
-
-def random_rain_speed():
-    return random.randint(5, FALLING_SPEED)
+from picographics import PicoGraphics, DISPLAY_TUFTY_2040
 
 class RainDrop:
-    def __init__(self, display, colour):
-        self.reset()
+    def __init__(self, display, colour, speed):
         self.display = display
+        WIDTH, HEIGHT = display.get_bounds()
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.speed = speed
         self.colour = colour
+        self.reset()
 
     def reset(self):
-        self.x = random_rain_x()
+        self.x = random.randint(0, self.width)
         self.y = 0
-        self.length = random_rain_length()
-        self.speed = random_rain_speed()
-        self.chars = [random_char()]
-        self.scale = random.randint(1, 2)
+        self.length = random.randint(5, 30)
+        self.speed = random.randint(5, self.speed)
+        self.chars = [self.random_char()]
+        self.scale = random.randint(1, 3)
         self.char_height = self.scale * 8
 
     def move(self):
         self.y += self.speed
 
     def draw(self):
-        # draw head
-        self.display.set_pen(WHITE)
-        self.chars.insert(0, random_char())
+        self.chars.insert(0, self.random_char())
         if len(self.chars) > self.length:
             self.chars.pop()
+        # draw head
+        self.display.set_pen(self.colour[0])
         self.display.character(self.chars[0], self.x, self.y, scale=self.scale)
         # draw tail
         length = len(self.chars)
@@ -45,19 +38,22 @@ class RainDrop:
             self.display.character(self.chars[i], self.x, self.y - (i * self.char_height), scale=self.scale)
 
     def is_offscreen(self):
-        return self.y > HEIGHT + self.length * self.char_height
-    
+        return self.y > self.height + self.length * self.char_height
+
     def tick(self):
         self.move()
         self.draw()
         if self.is_offscreen():
             self.reset()
 
+    def random_char(self):
+        return random.randint(33, 127)
+
 class Matrix:
-    def __init__(self, display, max, pens):# array of rain drops
+    def __init__(self, display, max_size, pens, max_speed):# array of rain drops
         self.drops = []
-        for _ in range(max):
-            self.drops.append(RainDrop(display, pens))
+        for _ in range(max_size):
+            self.drops.append(RainDrop(display, pens, max_speed))
 
     def draw(self):
         for drop in self.drops:
