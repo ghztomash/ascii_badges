6e767011f25ccb9f85a861d0d59fab6df78af0b1
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-24 02:39:39 +0100

    Clean up project

--- wavy_message.py
+++ wavy_message.py
@@ -1,27 +1,15 @@
 from picographics import PicoGraphics, DISPLAY_TUFTY_2040, PEN_RGB332
 import math, time
+from colours import hsv_to_rgb
 
 display = PicoGraphics(display=DISPLAY_TUFTY_2040, pen_type=PEN_RGB332, rotate=180)
 
-# convert a hue, saturation, and value into rgb values
-def hsv_to_rgb(h, s, v):
-    if s == 0.0: return v, v, v
-    i = int(h * 6.0)  
-    f = (h * 6.0) - i
-    p, q, t = v * (1.0 - s), v * (1.0 - s * f), v * (1.0 - s * (1.0 - f))
-    v, t, p, q = int(v * 255), int(t * 255), int(p * 255), int(q * 255)
-    i = i % 6
-    if i == 0: return v, t, p
-    if i == 1: return q, v, p
-    if i == 2: return p, v, t
-    if i == 3: return p, q, v
-    if i == 4: return t, p, v
-    if i == 5: return v, p, q
-    
-display.set_backlight(1.0)
-# display.set_font('bitmap8')
+WIDTH, HEIGHT = display.get_bounds()
 
-message = "0xDEAD 0xC0de 0xCAFE Raspberry Pi RP2040!"
+display.set_backlight(1.0)
+display.set_font('bitmap8')
+
+message = "DEAD C0DE CAFE!"
 text_size = 10
 message_width = display.measure_text(message, text_size)
 
@@ -33,13 +21,13 @@ while 1:
     display.clear()
 
     x_scroll -= 10
-    if x_scroll < -(message_width + 320 + 100):
-        x_scroll = 0
+    if x_scroll < -(message_width + 100):
+        x_scroll = WIDTH - 10
     
     # for each character we'll calculate a position and colour, then draw it
     for i in range(0, len(message)):
         cx = int(x_scroll + (i * text_size * 5.5))
-        cy = int(80 + math.sin(t * 10 + i) * 20)
+        cy = int(80 + math.sin(t * 10 + i) * 10)
         
         # to speed things up we only bother doing the hardware if the character will be visible on screen
         if cx > -50 and cx < 320:
@@ -53,4 +41,3 @@ while 1:
             display.text(message[i], cx, cy, -1, text_size)
 
     display.update()
-
