72055bf22e5a03a9d384f4dd0ced92fe09ed5d6b
Tomash Ghz <ghz.tomash@hotmail.com>
2023-12-24 18:20:39 +0100

    Ascii noise test

--- ascii_noise.py
+++ ascii_noise.py
@@ -1,6 +1,9 @@
 
 from picographics import PicoGraphics, DISPLAY_TUFTY_2040
 display = PicoGraphics(display=DISPLAY_TUFTY_2040)
+from noise import noise
+import random
+import time
 
 WIDTH, HEIGHT = display.get_bounds()
 
@@ -20,28 +23,43 @@ GREY = display.create_pen(33, 32, 32)
 MAGENTA = display.create_pen(255, 33, 140)
 CYAN = display.create_pen(33, 177, 255)
 
-# Uncomment one of these to change flag
-# If adding your own, colour order is left to right (or top to bottom)
-COLOUR_ORDER = [BLACK] 
-# COLOUR_ORDER = [VIOLET, BLACK] 
-# COLOUR_ORDER = [RED, ORANGE, YELLOW, GREEN, INDIGO, VIOLET]  # traditional pride flag
-# COLOUR_ORDER = [BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, INDIGO, VIOLET]  # Philadelphia pride flag
-# COLOUR_ORDER = [BLUE, PINK, WHITE, PINK, BLUE]  # trans flag
-# COLOUR_ORDER = [MAGENTA, YELLOW, CYAN]  # pan flag
-# COLOUR_ORDER = [MAGENTA, VIOLET, INDIGO]  # bi flag
-
 FONTS = ["bitmap6", "bitmap8", "bitmap14_outline", "sans", "gothic", "cursive", "serif", "serif_italic"]
+ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+def get_char_at_index(string, float_index):
+    index = int((len(string)-1) * float_index)
+    print(f"index: {index}")
+    return string[index]
+
+# fill the screen with ascii characters
+def ascii_noise(scale=1):
+    x = 0
+    y = 0
+    columns = (WIDTH // (9 * scale)) + 1
+    rows = HEIGHT // (9 * scale)
+    for i in range(columns * rows):
+        ic= random.randint(0, 128) / 128.0
+        print(f"ic: {ic}")
+        char = get_char_at_index(ascii_chars, ic)
+        display.set_pen(VIOLET)
+        display.rectangle(x, y, 8*scale, 8*scale)
+        display.set_pen(MAGENTA)
+        display.text(char, x, y, scale=scale)
+        x += 9 * scale
+        if x > WIDTH:
+            x = 0
+            y += 9 * scale
 
 # Change details here! Works best with a short, one word name
 TEXT = "37c3"
 
 display.set_font(FONTS[1])
 
-display.set_thickness(3)
-
 # draw background
-display.set_pen(MAGENTA)
-display.text(TEXT, 0, 0, scale=1, fixed_width=True)
+# display.set_pen(MAGENTA)
+# display.text(TEXT, 0, 0, scale=1, fixed_width=True)
+
+ascii_noise(2)
 
 # Once all the adjusting and drawing is done, update the display.
 display.update()
