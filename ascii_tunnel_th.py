a77534f5363e0f42ec4ad9683166a7df53ddff7d
Tomash Ghz <ghz.tomash@hotmail.com>
2024-01-17 00:35:35 +0100

    Add random movement and scaling to AsciiChain objects

--- ascii_tunnel_th.py
+++ ascii_tunnel_th.py
@@ -1,5 +1,6 @@
 import tuftyboard
 import time
+import random
 import colours
 import _thread
 import oscillator
@@ -20,25 +21,27 @@ FONTS = [
 ]
 
 # List of available pen colours, add more if necessary
-RED = display.create_pen(209, 34, 41)
-ORANGE = display.create_pen(246, 138, 30)
-YELLOW = display.create_pen(255, 216, 0)
-GREEN = display.create_pen(0, 121, 64)
-INDIGO = display.create_pen(36, 64, 142)
-VIOLET = display.create_pen(115, 41, 130)
+BACKGROUND = display.create_pen(0x28, 0x2a, 0x36)
+FOREGROUND = display.create_pen(0xf8, 0xf8, 0xf2)
+CYAN = display.create_pen(0x8b, 0xe9, 0xfd)
+GREEN = display.create_pen(0x50, 0xfa, 0x7b)
+ORANGE = display.create_pen(0xff, 0xb8, 0x6c)
+PINK = display.create_pen(0xff, 0x79, 0xc6)
+PURPLE = display.create_pen(0xbd, 0x93, 0xf9)
+RED = display.create_pen(0xff, 0x55, 0x55)
+YELLOW = display.create_pen(0xf1, 0xfa, 0x8c)
+
 WHITE = display.create_pen(255, 255, 255)
-PINK = display.create_pen(255, 175, 200)
-BLUE = display.create_pen(116, 215, 238)
-BROWN = display.create_pen(97, 57, 21)
 BLACK = display.create_pen(0, 0, 0)
-GREY = display.create_pen(32, 32, 32)
 MAGENTA = display.create_pen(255, 33, 140)
-CYAN = display.create_pen(33, 177, 255)
+BLUE = display.create_pen(116, 215, 238)
+VIOLET = display.create_pen(115, 41, 130)
+INDIGO = display.create_pen(36, 64, 142)
 
 # generate a list of pens with varying brightness values
-magenta = colours.Colour(255, 33, 140).set_saturation(1.0)
-pink = colours.Colour(255, 175, 200).set_saturation(1.0)
-violet = colours.Colour(115, 41, 130).set_saturation(1.0)
+magenta = colours.Colour(0xff, 0x55, 0x55)
+pink = colours.Colour(0xff, 0x79, 0xc6)
+violet = colours.Colour(0xbd, 0x93, 0xf9)
 PENS_MAGENTA = magenta.create_fade(display, 8)
 PENS_PINK = pink.create_fade(display, 8)
 PENS_VIOLET = violet.create_fade(display, 8)
@@ -47,7 +50,7 @@ PENS_VIOLET = violet.create_fade(display, 8)
 osc = oscillator.Oscillator(oscillator.fps_to_sample_rate(20), 80.0)
 center = Vector(WIDTH / 2, HEIGHT / 2)
 
-MAX_SIZE = 10
+MAX_SIZE = 1
 chains = []
 for i in range(MAX_SIZE):
     if i % 3 == 0:
@@ -56,7 +59,10 @@ for i in range(MAX_SIZE):
         pens = PENS_PINK
     else:
         pens = PENS_VIOLET
-    chains.append(AsciiChain(display, WHITE, pens, center, random_vector(5), length=4))
+    chains.append(AsciiChain(display, WHITE, pens, center, 
+                             random_vector(5),
+                             scale=random.uniform(1, 3),
+                             length=random.randint(5, 10)))
 
 
 lock = _thread.allocate_lock()
@@ -94,9 +100,13 @@ while True:
     tufty.draw_fps()
 
     lock.acquire()
+    source = chains[0].source
     for chain in chains:
         # draw particle
         chain.draw()
     lock.release()
+
+    display.set_pen(GREEN)
+    display.circle(int(source.x), int(source.y), 8)
     # Once all the adjusting and drawing is done, update the display.
     display.update()
